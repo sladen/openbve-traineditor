080f462b1075814cbf9022b09c149930d92e1dde
Michelle Boucquemont <reschanger@gmail.com>
2009-04-15 23:53:12 +0300

    import tool_traineditor_source.zip 1.0.3.0 (2009-04-12)

---
+++ TrainEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrainEditor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrainEditor")]
+[assembly: AssemblyCopyright("(AntiCopyright) 2009 openBVE Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("33e47c08-38d1-42ea-8c1a-47d5ed02a65e")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
